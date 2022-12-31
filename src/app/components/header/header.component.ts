8402ba655f0ede633a983e8351b8c71153107b0f
LSMarch <lsmarchetti01@gmail.com>
2022-12-30 21:01:49 -0600

    fixed header btn display/functionality

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -16,44 +16,6 @@ export class HeaderComponent implements OnInit {
   }
 
   ngOnInit(): void {
-  }
-  // test flags
-  private loggedIn: boolean = false;
-  private signUp: boolean = false;
-  private homepage: boolean = true;
-
-  // checking to see if buttons work
-  public getLoggedIn (): boolean {
-    return this.loggedIn;
-  }
-
-  public getSignedUp(): boolean {
-    return this.signUp;
-  }
-
-  public getHomepage(): boolean {
-    return this.homepage;
-  }
-
-  public setLoggedIn() {
-    this.loggedIn = true;
-    this.signUp = false;
-    this.homepage = false;
-    console.log('logged in');
-  }
-
-  public setSignup() {
-    console.log('got signup');
-    this.signUp = true;
-    this.loggedIn = false;
-    this.homepage = false;
-  }
-
-  public setHomepage() {
-    this.loggedIn = false;
-    this.signUp = false;
-    this.homepage = true;
-    console.log('we home')
-  }
+  } 
 
 }
