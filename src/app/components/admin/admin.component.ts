586cb28821a312fb06501c6afc87d221c9f0c267
David Villaron <david.villaron@outlook.com>
2023-01-04 10:34:57 -0600

    Added table of users to admin. Add & delete users

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,44 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {MatTableDataSource} from '@angular/material/table';
-
-// PLACE HOLDER DATA
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+import { pipe, Subscription, tap } from 'rxjs';
+// import { PageName } from 'src/app/enums/PageEnum';
+import { UiService } from 'src/app/services/ui.service';
+import { AppUser } from 'src/data/User';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
-export class AdminComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+export class AdminComponent implements OnInit, OnDestroy {
+  displayedColumns: string[] = ['delete','id','role', 'email', 'password', 'save'];
+  users: AppUser[] = [];
+  appUsers$ = this.ui.getAppUsers$
+  dataSource: any;
+  adminSub: Subscription;
+  
+  //input fields
+  email: string = '';
+  password: string = '';
+  newUserType: string = '';
+  customer: string = '';
+  shopkeeper: string = '';
+  admin: string='';
+
+
+  // constructor(public ui: UiService) {
+  //   this.adminSub = this.appUsers$.subscribe(user => {
+  //     this.users = user
+  //     this.dataSource = new MatTableDataSource(this.users)
+  //   })
+  // }
+  constructor(public ui: UiService) {
+    this.adminSub = ui.whenAppUsersUpdates().subscribe((user) => {
+      console.log('user: ', user)
+      this.users = user
+      this.dataSource = new MatTableDataSource(user)
+    })
+    ui.loadUsers();
+  }
+
+  ngOnInit(): void {
+  }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
-  ngOnInit(): void {
+ 
+  onEdit(): void{
+    console.log('edit icon clicked')
   }
 
   
+  
+  ngOnDestroy(): void {
+    this.adminSub.unsubscribe();
+   }
 
 }
